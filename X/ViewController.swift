e05d070d33acf8faae429e12caddc72e69eab79e
Gaurav Nayak <gauravnayak@iiitdmj.ac.in>
2018-01-06 12:04:32 +0530

    Initial Commit

---
+++ X/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  X
+//
+//  Created by GAURAV NAYAK on 06/01/18.
+//  Copyright © 2018 GAURAV NAYAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
